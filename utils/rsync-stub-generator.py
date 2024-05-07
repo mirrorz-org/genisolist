eb93460ea2c40f03cc518c501c37177e156e3eb0
taoky <me@taoky.moe>
2024-05-08 00:07:44 +0800

    fix: handle symlink in rsync stub generator

--- utils/rsync-stub-generator.py
+++ utils/rsync-stub-generator.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 def main(upstream: str, dist: Path) -> None:
     p = subprocess.run(
-        ["rsync", "--no-motd", "-r", "--list-only", upstream],
+        ["rsync", "-a", "--no-motd", "-r", "--list-only", upstream],
         stdout=subprocess.PIPE,
         text=True,
     )
@@ -17,12 +17,30 @@ def main(upstream: str, dist: Path) -> None:
         exit(1)
     for line in p.stdout.splitlines():
         line = line.strip()
-        splitted = line.split(maxsplit=5)
+        splitted = line.split(maxsplit=4)
         perm = splitted[0]
         path = splitted[-1]
-        is_dir = perm.startswith("d")
-        if is_dir:
+        if perm.startswith("d"):
             dist.joinpath(path).mkdir(parents=True, exist_ok=True)
+        elif perm.startswith("l"):
+            # well that's a bit tricky
+            path = path.split(" -> ", maxsplit=1)
+            target = path[1]
+            symlink = dist.joinpath(path[0])
+            try:
+                symlink.symlink_to(target)
+            except FileExistsError:
+                # check if the symlink is the same as the target
+                if symlink.is_symlink():
+                    symlink_target = symlink.readlink()
+                    if str(symlink_target) == target:
+                        continue
+                    else:
+                        print(symlink_target, target)
+                print("Symlink already exists -- maybe you should nuke the dist directory first?")
+                print("Symlink:", symlink)
+                print("Target:", target)
+                exit(-1)
         else:
             dist.joinpath(path).touch()
 
